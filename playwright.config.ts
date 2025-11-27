be5285b8c17520642e1ad30101d38cef7cdfb8e3
Dinesh <mlnarasimhaiah@gmail.com>
2025-11-27 14:07:34 +0530

    Modified playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -48,7 +48,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot:'on',
     video:'on',
-    headless:false    
+    headless:true    
   },
   //This is used to save the username and password
   metadata:{
