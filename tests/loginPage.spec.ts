7c921c6c224b054fe6170da94a7cd781e3ec0876
Dinesh <mlnarasimhaiah@gmail.com>
2025-12-04 12:57:42 +0530

    added jenkins file

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -12,10 +12,10 @@ test('Verify login to an application with valid credentials @sanity',
     await expect(homePage.page).toHaveTitle('My Account');
 });
 
-test.skip('Verify the error message for invalid login credentials',async({page,baseURL})=>{
-    const loginPage=new LoginPage(page);
-    await loginPage.goto(baseURL);
-    await loginPage.doLogin('diddne3sdh23@mail.com','1@Dnsde3cdsh');
-    const errorMessage=await loginPage.invalidLogin();
-    expect(errorMessage).toContain('Warning: No match for E-Mail Address and/or Password.');
-});
+// test.skip('Verify the error message for invalid login credentials',async({page,baseURL})=>{
+//     const loginPage=new LoginPage(page);
+//     await loginPage.goto(baseURL);
+//     await loginPage.doLogin('diddne3sdh23@mail.com','1@Dnsde3cdsh');
+//     const errorMessage=await loginPage.invalidLogin();
+//     expect(errorMessage).toContain('Warning: No match for E-Mail Address and/or Password.');
+// });
